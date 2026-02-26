f746e5f91953060b57b3f7f8f42563f2954a7289
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2026-02-26 14:42:50 -0800

    api ref updates

--- great_expectations/datasource/fluent/sql_server_datasource.py
+++ great_expectations/datasource/fluent/sql_server_datasource.py
@@ -164,9 +164,18 @@ class SQLServerDatasource(SQLDatasource):
 
     Args:
         name: The name of this SQL Server datasource.
-        connection_string: Structured connection details for SQL Server.
-            Alternatively, pass connection detail fields (host, database, schema,
-            username, password, etc.) as keyword arguments directly.
+        host: The environment where the Microsoft SQL Server engine is installed and running, for example "sql-server.example.com" for a self-hosted Microsoft SQL Server instance.
+        database: The name of the Microsoft SQL Server database where the data you want to validate is stored.
+        schema: The name of the Microsoft SQL Server schema where the data you want to validate is stored.
+        port: The port configured for your Microsoft SQL Server instance, typically 1433.
+        encrypt: The TLS encryption protocol to use. Accepts the following: "Optional" - Establish an encrypted connection if your Microsoft Fabric instance is configured to force encryption. Otherwise, establish an unencrypted connection. "Mandatory" - Require the connection to be encrypted. Connection will fail if your Microsoft Fabric instance does not support TLS. "Strict" - Require the connection to be encrypted and validate the server certificate. Connection will fail if your Microsoft Fabric instance does not support TLS or the certificate is not valid.
+        driver: The name of the ODBC driver your environment uses to connect to Microsoft Fabric. Common values include the following: "ODBC Driver 18 for SQL Server", "ODBC Driver 17 for SQL Server", and "FreeTDS".
+        authentication: Accepts "SQL Server" or "Entra ID". Required credential parameters depend on the authentication method.
+        username: For "SQL Server" authentication. The username you use to access Microsoft SQL Server.
+        password: For "SQL Server" authentication. The password you use to access Microsoft SQL Server.
+        tenant_id: For "Entra ID" authentication. The unique identifier for your organization's instance of Microsoft Entra ID.
+        client_id: For "Entra ID" authentication. The application ID for your new or existing app registration.
+        client_secret: For "Entra ID" authentication. A new secret key from your app registration.
         assets: An optional dictionary whose keys are TableAsset or QueryAsset names and whose
             values are TableAsset or QueryAsset objects.
     """
