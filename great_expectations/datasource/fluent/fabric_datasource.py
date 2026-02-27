e9d1edb8e8560b1d4904958923a1f1b3684f4405
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2026-02-27 07:07:48 -0800

    line length

--- great_expectations/datasource/fluent/fabric_datasource.py
+++ great_expectations/datasource/fluent/fabric_datasource.py
@@ -29,14 +29,36 @@ class FabricDatasource(SQLServerDatasource):
 
     Args:
         name: The name of this Fabric datasource.
-        host: Your Microsoft Fabric workload endpoint, for example "myworkspace.datawarehouse.fabric.microsoft.com" or "abc123.database.fabric.microsoft.com".
-        database: The name of the Microsoft Fabric database where the data you want to validate is stored.
-        schema: The name of the Microsoft Fabric schema where the data you want to validate is stored.
-        port: The port configured for your Microsoft Fabric instance, typically 1433.
-        encrypt: The TLS encryption protocol to use. Accepts the following. "Optional" - Establish an encrypted connection if your Microsoft Fabric instance is configured to force encryption. Otherwise, establish an unencrypted connection. "Mandatory" - Require the connection to be encrypted. Connection will fail if your Microsoft Fabric instance does not support TLS. "Strict" - Require the connection to be encrypted and validate the server certificate. Connection will fail if your Microsoft Fabric instance does not support TLS or the certificate is not valid.
-        driver: The name of the ODBC driver your environment uses to connect to Microsoft Fabric. Common values include the following: "ODBC Driver 18 for SQL Server", "ODBC Driver 17 for SQL Server", and "FreeTDS".
-        tenant_id: The unique identifier for your organization's instance of Microsoft Entra ID.
-        client_id: The application ID for your new or existing Entra ID app registration.
+        host: Your Microsoft Fabric workload endpoint,
+            for example "myworkspace.datawarehouse.fabric.microsoft.com"
+            or "abc123.database.fabric.microsoft.com".
+        database: The name of the Microsoft Fabric database
+            where the data you want to validate is stored.
+        schema: The name of the Microsoft Fabric schema
+            where the data you want to validate is stored.
+        port: The port configured for your Microsoft Fabric instance,
+            typically 1433.
+        encrypt: The TLS encryption protocol to use.
+            Accepts the following.
+            - "Optional" - Establish an encrypted connection if your
+            Microsoft Fabric instance is configured to force encryption.
+            Otherwise, establish an unencrypted connection.
+            - "Mandatory" - Require the connection to be encrypted.
+            Connection will fail if your Microsoft Fabric instance
+            does not support TLS.
+            - "Strict" - Require the connection to be encrypted and
+            validate the server certificate. Connection will fail if
+            your Microsoft Fabric instance does not support TLS
+            or the certificate is not valid.
+        driver: The name of the ODBC driver your environment uses to
+            connect to Microsoft Fabric. Common values include:
+            - "ODBC Driver 18 for SQL Server"
+            - "ODBC Driver 17 for SQL Server"
+            - "FreeTDS".
+        tenant_id: The unique identifier for your organization's
+            instance of Microsoft Entra ID.
+        client_id: The application ID for your new or existing
+            Entra ID app registration.
         client_secret: A new secret key from your Entra ID app registration.
         assets: An optional dictionary whose keys are TableAsset or QueryAsset names and whose
             values are TableAsset or QueryAsset objects.
